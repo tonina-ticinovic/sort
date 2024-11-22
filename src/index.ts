488144a842d658eba18f2709a7a41b9e096ac25e
Tonina Tičinović <toninaticinovic2@gmail.com>
2024-11-22 13:12:43 +0100

    Add linked list

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, -100]);
-console.log(numbersCollection.data);
+// console.log(numbersCollection.data);
 
 const arbitraryCollection = new CharactersCollection('Xbaac');
-console.log(arbitraryCollection.data);
+// console.log(arbitraryCollection.data);
 
 const sorterNumbers = new Sorter(numbersCollection);
 const sorterString = new Sorter(arbitraryCollection);
 sorterNumbers.sort();
 sorterString.sort();
-console.log(sorterNumbers.collection);
-console.log(sorterString.collection);
+// console.log(sorterNumbers.collection);
+// console.log(sorterString.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorterLinkedList = new Sorter(linkedList);
+sorterLinkedList.sort();
+linkedList.print();
